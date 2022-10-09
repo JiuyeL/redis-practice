4fa609a2a847b49c6dd16f23a2988b6f074df45f
李东阳 <ldy996@qq.com>
2022-10-09 20:46:58 +0800

    初始化项目

---
+++ src/test/java/com/dy/redispractice/RedisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dy.redispractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
