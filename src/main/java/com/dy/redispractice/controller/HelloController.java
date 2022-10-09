e9e88ff68cb44ab26bdb74814b14fc2c15aa829d
李东阳 <ldy996@qq.com>
2022-10-09 21:26:34 +0800

    初始化redis项目

---
+++ src/main/java/com/dy/redispractice/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.dy.redispractice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dongyang
+ * @date 2022/10/9 20:53
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @GetMapping("/hello")
+    public String test(){
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        String hello = valueOperations.get("hello");
+        return hello;
+    }
+}
